f2266d68b55794a1c025602304d1bfd839448582
Devesh Singh <devesh.nttdata@gmail.com>
2019-11-26 08:42:17 +0530

    refactored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'app' },
+  { path: 'app', component: AppComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './nav-bar/navbar/navbar.component';
-import { HomeHeaderComponent } from './home-header/home-header/home-header.component';
-import { HomeIntroComponent } from './home-intro/home-intro/home-intro.component';
-import { HomeCodeShopComponent } from './home-code-shop/home-code-shop/home-code-shop.component';
-import { HomeProjectsComponent } from './home-projects/home-projects/home-projects.component';
-import { HomeVideoComponent } from './home-video/home-video.component';
-import { HomeHireMeComponent } from './home-hire-me/home-hire-me.component';
-import { HomeAboutComponent } from './home-about/home-about.component';
-import { HomePostsComponent } from './home-posts/home-posts.component';
-import { FooterComponent } from './footer/footer.component';
+
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { NavbarComponent } from './root/navbar/navbar.component';
+import { HomeHeaderComponent } from './root/home-header/home-header.component';
+import { HomeIntroComponent } from './root/home-intro/home-intro.component';
+import { HomeCodeShopComponent } from './root/home-code-shop/home-code-shop.component';
+import { HomeProjectsComponent } from './root/home-projects/home-projects.component';
+import { HomeVideoComponent } from './root/home-video/home-video.component';
+import { HomeHireMeComponent } from './root/home-hire-me/home-hire-me.component';
+import { HomeAboutComponent } from './root/home-about/home-about.component';
+import { HomePostsComponent } from './root/home-posts/home-posts.component';
+import { FooterComponent } from './root/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,9 @@ import { FooterComponent } from './footer/footer.component';
     HomeHireMeComponent,
     HomeAboutComponent,
     HomePostsComponent,
-    FooterComponent
+    FooterComponent,
+    ResumeComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
